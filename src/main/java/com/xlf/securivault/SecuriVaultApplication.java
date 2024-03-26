20dfdf06d95cd962c1cef16b81cf2565ac4a2d58
XiaoLFeng <gm@x-lf.cn>
2024-03-26 15:59:26 +0800

    Initial commit

---
+++ src/main/java/com/xlf/securivault/SecuriVaultApplication.java
@@ -0,0 +1,20 @@
+package com.xlf.securivault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Springboot启动
+ *
+ * @author xiao_lfeng
+ * @since v1.0.0
+ * @version v1.0.0
+ */
+@SpringBootApplication
+public class SecuriVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuriVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xlf/securivault/SecuriVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlf.securivault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuriVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
